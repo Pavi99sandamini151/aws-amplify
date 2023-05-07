4a5ec095c3f181bdb16c401262e94e3e57a491c9
Pavi99 <pavithra@smbapps.com.au>
2023-05-07 06:29:40 +0530

    added amplify configurarion

--- amplify-react-app/src/amplify.js
+++ amplify-react-app/src/amplify.js
@@ -1,4 +1,10 @@
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsConfig from './aws-exports';
 
 Amplify.configure(awsConfig);
+
+const awsmobile = {
+  "aws_project_region": "ap-northeast-1"
+};
+
+export default awsmobile;
